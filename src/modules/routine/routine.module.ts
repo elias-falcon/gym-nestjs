0824e87247cb96fe032ec0f200a55f93affb2484
Falcón Elias <felias.ezequiel@gmail.com>
2020-05-26 12:38:22 -0300

    [ADD]: Add co, s and dtos in Routine

--- src/modules/routine/routine.module.ts
+++ src/modules/routine/routine.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RoutineRepository } from './routine.repository';
+import { RoutineController } from './routine.controller';
+import { RoutineService } from './routine.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([RoutineRepository])]
+    imports: [TypeOrmModule.forFeature([RoutineRepository])],
+    controllers: [RoutineController],
+    providers: [RoutineService]
 
 })
 export class RoutineModule {}
