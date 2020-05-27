49e680cfa8425781a5c98e4c20d9a2e9efbe7f45
Falcón Elias <felias.ezequiel@gmail.com>
2020-05-27 13:59:52 -0300

    [REF]: Refactoring controller

--- src/modules/state-user-entity/state-user-entity.controller.ts
+++ src/modules/state-user-entity/state-user-entity.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Patch, Delete, ParseIntPipe } from '@nestjs/common';
+import { StateUserEntityRepository } from './state-user-entity.respository';
+import { ReadStateUserDto, CreateStateUserDto, UpdateStateUserDto } from './dtos';
 
 @Controller('state-user-entity')
-export class StateUserEntityController {}
+export class StateUserEntityController {
+
+        constructor(private readonly _stateUserRepository: StateUserEntityRepository){}
+    
+        @Get(':stateUserId')
+        async getStateUser(@Param('stateUserId', ParseIntPipe) stateUserId: number): Promise<ReadStateUserDto>{
+            return this._stateUserRepository.get(stateUserId);
+        }
+    
+        @Get()
+        async getAll(): Promise<ReadStateUserDto[]>{
+            return this._stateUserRepository.getAll();
+        }
+    
+        @Post()
+        createStateUser (@Body() state: Partial<CreateStateUserDto> ): Promise<ReadStateUserDto>{
+            return this._stateUserRepository.create(state);
+        }
+    
+        @Patch(':stateUserId')
+         updateStateUser (@Param('stateUserId', ParseIntPipe) stateUserId: number, @Body() state: Partial<UpdateStateUserDto> ){
+            return this._stateUserRepository.update(stateUserId, state);
+        }
+    
+        @Delete(':stateUserId')
+         deleteRole(@Param('stateUserId', ParseIntPipe) stateUserId: number){
+            return this._stateUserRepository.delete(stateUserId);
+        }
+    }
+    
+
+
+}
